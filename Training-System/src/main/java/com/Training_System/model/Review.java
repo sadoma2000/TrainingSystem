d95885e3e9a1fabe3e60577179ea4b0229be321c
sadoma2000 <sadoma11022@gmail.com>
2024-09-07 22:30:07 +0300

    New

--- Training-System/src/main/java/com/Training_System/model/Review.java
+++ Training-System/src/main/java/com/Training_System/model/Review.java
@@ -1,6 +1,5 @@
 package com.Training_System.model;
 
-<<<<<<< HEAD
 import java.sql.Timestamp;
 
 import jakarta.persistence.Entity;
@@ -35,28 +34,3 @@ public class Review {
 	private Timestamp Review_date;
 
 }
-=======
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Review {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private Long studentId;
-    private Long courseId;
-    private String reviewText;
-    private int rating;
-}
->>>>>>> branch 'master' of https://github.com/sadoma2000/TrainingSystem.git
