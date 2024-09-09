a43d9170d43819c1aad966ca06f48bca64f8a3e3
murouj <Murouj.alshahri@gmail.com>
2024-09-09 19:22:09 +0300

    add commit

--- Training-System/src/main/java/com/Training_System/model/Certificate.java
+++ Training-System/src/main/java/com/Training_System/model/Certificate.java
@@ -17,7 +17,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Certificate {
-
+	//
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
