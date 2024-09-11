cf0784d89fe67aca10ddf3175f35433edb11a3cb
sadoma2000 <sadoma11022@gmail.com>
2024-09-11 17:31:27 +0300

    fixedByS

--- Training-System/src/main/java/com/Training_System/Config/ConfigSecurity.java
+++ Training-System/src/main/java/com/Training_System/Config/ConfigSecurity.java
@@ -35,7 +35,7 @@ public class ConfigSecurity {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
                 .authenticationProvider(daoAuthenticationProvider())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/v1/auth/**").permitAll() // Allowed for ALL
+                        .requestMatchers("/api/auth/**").permitAll() // Allowed for ALL
                         .requestMatchers(
                                 "/api/certificates",
                                 "/api/enrollments/delete/{id}",
@@ -54,7 +54,7 @@ public class ConfigSecurity {
                                 "/api/students/get-all")
                         .hasAuthority("Instructor")
                         .requestMatchers(
-                                "/api/v1/Instructors/get-all",
+                                "/api/Instructors/get-all",
                                 "/api/students/get-all",
                                 "/api/Course/get-all",
                                 "/api/lessons/get-all",
